d72125ada1f92bad8d57449241155239c3d1d27f
Anna Zhang <annayzhang@fb.com>
2020-08-05 08:58:49 -0700

    updated index server

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.MONGODB_URI || "mongodb://heroku_dhmz5wzn:fnkat1112h7vu7prchka0a14pk@ds137891.mlab.com:37891/heroku_dhmz5wzn";
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+var pushConfig = {};
+
+if (process.env.FCM_API_KEY) {
+    pushConfig['android'] = { apiKey: process.env.FCM_API_KEY || ''};
+}
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'votingApp',
+  masterKey: process.env.MASTER_KEY || 'votingAppAndroidFBU', //Add your master key here. Keep it secret!
+  push: pushConfig,
+  serverURL: process.env.SERVER_URL || 'https://voting-app-fbu.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
